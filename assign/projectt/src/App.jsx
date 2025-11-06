87144b451849e72997d46115a62e47cbec801e68
Shiwani029 <Shiwanisep29@gmail.com>
2025-11-06 10:14:10 +0530

    hello

--- assign/projectt/src/App.jsx
+++ assign/projectt/src/App.jsx
@@ -1,18 +1,24 @@
-
+import { Link, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Count from './Components/count'
+import Login from './Components/login'
+import Register from './Components/register'
+
 function App() {
-
   return (
-    <>
     <div className='App'>
-      
-       <Count/>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/login">Login</Link> |{" "}
+        <Link to="/register">Register</Link>
+      </nav>
 
-      </div>
-     
-
-    </>
+      <Routes>
+        <Route path="/" element={<Count />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ assign/projectt/src/Components/login.jsx
@@ -0,0 +1,14 @@
+// Components/login.jsx
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+export default Login
